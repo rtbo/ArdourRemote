9b0fc02f9f770782ec9e02157e0739fb11981eaf
Remi Thebault <remi.thebault@gmail.com>
2021-12-29 18:57:05 +0100

    button create new connection

--- app/src/main/java/rtbo/ardourremote/view/ConnectActivity.kt
+++ app/src/main/java/rtbo/ardourremote/view/ConnectActivity.kt
@@ -1,12 +1,11 @@
 package rtbo.ardourremote.view
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dagger.hilt.android.AndroidEntryPoint
 import rtbo.ardourremote.R
 
@@ -21,11 +20,20 @@ class ConnectActivity : AppCompatActivity() {
         val listAdapter = ConnectionListAdapter()
         viewModel.connections.observe(this, {
             if (it.isEmpty()) {
-                val intent = Intent(this, ConnectNewActivity::class.java)
-                startActivity(intent)
+                startConnectNewActivity()
             }
             listAdapter.itemViewModels = it
         })
         list.adapter = listAdapter
+
+        val create = findViewById<FloatingActionButton>(R.id.create_new_btn)
+        create.setOnClickListener {
+            startConnectNewActivity()
+        }
+    }
+
+    private fun startConnectNewActivity() {
+        val intent = Intent(this, ConnectNewActivity::class.java)
+        startActivity(intent)
     }
 }
